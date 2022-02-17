3d7e7cddf8cd18584fc39812f054a7a7a5a485b2
Marc Fresquet Rius <marc@driviesapp.com>
2022-02-17 02:09:17 +0100

    Update tokens to pay amount

--- pay_rewards/variables.py
+++ pay_rewards/variables.py
@@ -1,6 +1,6 @@
 my_wallet = "6HNCL2A5RZJN5LLTBB76NUCRM5TRFGMTSPIXT4JTP3A2WXZZQZKYNLP4HI"
 my_token_id = "65875461"  # ASA identifier
-total_tokens_to_pay = 2_000_000_000  # in micros
+total_tokens_to_pay = 20_000_000_000  # in micros
 txn_start = "2022-02-07"
 txn_end = "2022-02-14"
 
